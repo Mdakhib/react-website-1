5d9b7bdd897499e16c56f7c692a032ed29758047
Mdakhib <mohamedakhib@gmail.com>
2021-05-30 14:55:27 +0530

    Hero section created and added to the home.js to render that by using router

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="hero-container">
-      <video src="/public/videos/video-2.mp4" autoPlay loop></video>
+      <video src="/videos/video-2.mp4" autoPlay loop></video>
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
