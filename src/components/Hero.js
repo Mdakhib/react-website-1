9d3b7ee0c2f4d58b6282e0fe09d6c4fea49bd960
Mdakhib <mohamedakhib@gmail.com>
2021-05-30 15:30:53 +0530

    css added to the Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,13 +6,13 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop></video>
+      <video src="/videos/video-2.mp4" autoPlay loop muted></video>
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
         <Button
           className="btns"
-          buttonStyles="btn--outline"
+          buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
           GET STARTED
