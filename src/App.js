00654df9daa556857f6a6cab1bf6f54d449b7235
Mdakhib <mohamedakhib@gmail.com>
2021-05-30 17:59:35 +0530

    router added to the pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Home from '../src/components/pages/Home'
+import Services from '../src/components/pages/Services'
+import Products from "../src/components/pages/Products";
+import SignUp from "../src/components/pages/SignUp";
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 function App() {
@@ -9,7 +12,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
